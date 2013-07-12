30785138c207f9113454c223e85dbe6fc7731c5f
Sean "CyberKitsune" McClenaghan <cyberkitsune09@gmail.com>
2013-07-12 00:26:08 -0700

    Made logging not look terrible

--- src/net/cyberkitsune/jCube/Server.java
+++ src/net/cyberkitsune/jCube/Server.java
@@ -2,6 +2,7 @@ package net.cyberkitsune.jCube;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.logging.ConsoleHandler;
 import java.util.logging.Logger;
 
 import net.cyberkitsune.jCube.protocol.NetworkProtocol;
@@ -9,10 +10,18 @@ import net.cyberkitsune.jCube.protocol.Version3;
 
 public class Server {
 
-	public Logger log = Logger.getLogger("jCube");
+	public static final Logger log = Logger.getLogger(Server.class.getName());
 	public NetworkProtocol activeProto = new Version3(this);
 	boolean running = true;
 	int numConnections = -1;
+	
+	static {
+		LogFormatter formatter = new LogFormatter();
+		ConsoleHandler cH = new ConsoleHandler();
+		cH.setFormatter(formatter);
+		log.setUseParentHandlers(false);
+		log.addHandler(cH);
+	}
 	
 	public void run() throws IOException {
 		log.info("Starting Server on Port 12345");
