f106f76e0c1635e725b976932876edaa136ad20e
Sean "CyberKitsune" McClenaghan <cyberkitsune09@gmail.com>
2013-07-14 02:04:16 -0700

    Todo list

--- src/net/cyberkitsune/jKube/JKube.java
+++ src/net/cyberkitsune/jKube/JKube.java
@@ -1,5 +1,19 @@
 package net.cyberkitsune.jKube;
 
+/**
+ * JKube - Java CubeWorld Server
+ * TODO List (In order of importance)
+ * * Structures
+ * * Packet Logic
+ * * Server... stuff? (Map simulation, entity management, etc)
+ * * World Generation
+ * * World parsing / saving
+ * * Event Management
+ * * APIs / Plugins
+ * @author Sean McClenaghan, Doridian
+ *
+ */
+
 public class JKube {
 
 	public static void main(String[] args) {
