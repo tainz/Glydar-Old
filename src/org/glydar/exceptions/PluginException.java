603f11c5a511b78162be6a08a380fdf91f0aa73e
Sean "CyberKitsune" McClenaghan <cyberkitsune09@gmail.com>
2013-07-17 23:07:43 -0700

    Fixed some Java warnings

--- src/org/glydar/exceptions/PluginException.java
+++ src/org/glydar/exceptions/PluginException.java
@@ -3,6 +3,11 @@ package org.glydar.exceptions;
 public class PluginException extends Exception
 {
 
+	/**
+	 * TODO
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public PluginException(String message)
 	{
 		super(message);
--- src/org/glydar/exceptions/StructureMismatchException.java
+++ src/org/glydar/exceptions/StructureMismatchException.java
@@ -3,6 +3,11 @@ package org.glydar.exceptions;
 public class StructureMismatchException extends Exception
 {
 	
+	/**
+	 * TODO
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public StructureMismatchException(String message)
 	{
 		super(message);
