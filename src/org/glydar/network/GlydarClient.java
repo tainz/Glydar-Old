e6c61a4c89d5e10fbd0a9d57de82d13ed8e9bc63
Sean "CyberKitsune" McClenaghan <cyberkitsune09@gmail.com>
2013-07-18 22:54:15 -0700

    Added connection boolean in GlydarClient.

--- src/org/glydar/network/GlydarClient.java
+++ src/org/glydar/network/GlydarClient.java
@@ -7,12 +7,14 @@ import java.net.InetSocketAddress;
 public class GlydarClient {
 
     private int id;
+    private boolean connected;
 
     private SocketChannel channel;
 
     public GlydarClient(int id, SocketChannel channel) {
         this.id = id;
         this.channel = channel;
+        this.connected = false;
     }
 
     public int getId() {
@@ -25,6 +27,14 @@ public class GlydarClient {
 
     public SocketChannel getSocketChannel() {
         return channel;
+    }
+
+    public boolean isConnected() {
+        return connected;
+    }
+
+    public void setConnected(boolean c) {
+        connected = c;
     }
 
 }
